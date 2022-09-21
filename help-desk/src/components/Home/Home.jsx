5859c5f235510e145e62c70b5858cac9ff973e1a
shivghari <shiv.i.ghariwala@gmail.com>
2022-09-21 19:18:17 +0530

    Home Page UI Optimization

--- help-desk/src/components/Home/Home.jsx
+++ help-desk/src/components/Home/Home.jsx
@@ -1,50 +1,71 @@
-
-import { Grid } from '@mui/material'
-import React from 'react'
-import './Home.css'
+import { Grid } from "@mui/material";
+import React from "react";
+import "./Home.css";
+import AddLocationAltIcon from "@mui/icons-material/AddLocationAlt";
 
 function Home() {
   return (
-    <div className='home-container'>
-        <div className='cgpit-slider'>
-            <img src='./images/CGPIT1.png' alt='cgpit' className='cgpit-slider-image'/>    
-        </div>  
-        <div className='customer-journy-map'>
-            <img src='./images/customerJourneyMap.png' alt='customer Journey Map'/>
-            <div>
-                <p>
-                    348J+X6M, Maliba Campus, Mahuva-Bardoli Rd, Gopal Vidyanagar, Tarsadi, Gujarat 394350
-                </p>
-                <p>
-                    CGPIT - ChhotuBhai Gopalbhi Patel Institute of Technology
-                </p>
-            </div>
+    <div className="home-container">
+      <div className="cgpit-slider">
+        <img
+          src="./images/CGPIT.jpg"
+          alt="cgpit"
+          className="cgpit-slider-image"
+        />
+      </div>
+      <div className="customer-journy-map">
+        {/* <img src="./images/customerJourneyMap.png" alt="customer Journey Map" /> */}
+        <AddLocationAltIcon fontSize="large" />
+        <div>
+          <p>
+            348J+X6M, Maliba Campus, Mahuva-Bardoli Rd, Gopal Vidyanagar,
+            Tarsadi, Gujarat 394350
+          </p>
+          <p>CGPIT - ChhotuBhai Gopalbhi Patel Institute of Technology</p>
         </div>
-        <div className='title'>
-            <h1>Ease Your Task...We will Help you...</h1>
-        </div>
-        
-        <div className='portal-container'>
-            <h3>Who are You ?</h3>
-            <br/>
-            <Grid container spacing={2}>
-                <Grid item md={4} sm={4} xs={12} >
-                    <img src='./images/studentPortal.png' alt='studentPortal.png' width='100%' height='75%'/>
-                    <h2>Student Portal</h2>
-                    
-                </Grid>
-                <Grid item md={4} sm={4} xs={12}>
-                    <img src='./images/facultyPortal.png' alt='studentPortal.png' width='100%' height='75%'/>
-                    <h2>Faculty Portal</h2>
-                </Grid>
-                <Grid item md={4} sm={4} xs={12}>
-                    <img src='./images/other.png' alt='studentPortal.png' width='100%' height='75%'/>
-                    <h2>Other</h2>
-                </Grid>
-            </Grid>
-        </div>
+      </div>
+      <div className="title">
+        <h1>Ease Your Task...We will Help you...</h1>
+      </div>
+
+      <div className="portal-container">
+        <h3>Who are You ?</h3>
+        <br />
+        <Grid container spacing={2}>
+          <Grid item md={4} sm={4} xs={12}>
+            <img
+              src="./images/StudentPortal.jpg"
+              alt="studentPortal.png"
+              width="100%"
+              height="75%"
+              className="portalImage"
+            />
+            <h2>Student Portal</h2>
+          </Grid>
+          <Grid item md={4} sm={4} xs={12}>
+            <img
+              src="./images/FacultyPortal.jpg"
+              alt="studentPortal.png"
+              width="100%"
+              height="75%"
+              className="portalImage"
+            />
+            <h2>Faculty Portal</h2>
+          </Grid>
+          <Grid item md={4} sm={4} xs={12}>
+            <img
+              src="./images/OtherPortal.jpg"
+              alt="studentPortal.png"
+              width="100%"
+              height="75%"
+              className="portalImage"
+            />
+            <h2>Other</h2>
+          </Grid>
+        </Grid>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
