2aebbb8b52874e2722a7b9ba428b1e34ec61b04f
Pateldarshan29 <pateldarshan2910@gmail.com>
2022-09-23 09:17:44 -0700

    LoginForm added and routing done for login and signup form

--- help-desk/src/App.js
+++ help-desk/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 import StudentSignin from "./components/SigninForms/StudentSignin/StudentSignin";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
+import Login from "./components/LoginForm/Login"
 import Complaint from "../src/components/Complaint";
 
 //routing imports
@@ -17,7 +18,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
-          <Route exact path="/signin" element={<StudentSignin />}></Route>
+          <Route exact path="/signin" element={<Login />}></Route>
+          <Route exact path="/signup" element={<StudentSignin />}></Route>
           <Route exact path="/complaint" element={<Complaint />}></Route>
           <Route exact path="/profile" element={<Profile />}></Route>
         </Routes>
