23af7777a30033991170ab819ec6357b63d73bd3
shivghari <shiv.i.ghariwala@gmail.com>
2022-10-27 13:14:18 +0530

    Post Information Component Is Integrated

--- backend/Routers/InformationPortal.js
+++ backend/Routers/InformationPortal.js
@@ -1,39 +1,69 @@
 const express = require("express");
 const router = express.Router();
 const InformationSchema = require("../Models/InformationSchema");
+const multer = require("multer");
+const upload = multer({ dest: "InformationPhoto" });
+const fs = require("fs");
+const path = require("path");
 
-router.post("/postinformation",async(req,res)=>{
-    try{
+router.post(
+  "/postinformation",
+  upload.single("infoPhoto"),
+  async (req, res) => {
+    try {
+      if (req.file) {
+        let fileType = req.file.mimetype.split("/")[1];
+        let newFilename = req.file.filename + "." + fileType;
+        fs.rename(
+          path.resolve(process.cwd(), `InformationPhoto/${req.file.filename}`),
+          path.resolve(process.cwd(), `InformationPhoto/${newFilename}`),
+          (data) => {
+            console.log("File Uploaded: ", newFilename);
+          }
+        );
         const information = InformationSchema({
-            title: req.body.title,
-            author: req.body.author,
-            desc: req.body.desc,
-            image: req.body.image,
-            mention: req.body.mention
-        })
-        const createinformation= await information.save()
-        res.status(200).send(createinformation)  
-    }catch(e){
-        res.status(400).send(e)
+          title: req.body.title,
+          author: req.body.author,
+          desc: req.body.desc,
+          image: newFilename,
+          mention: req.body.mention,
+        });
+        const createinformation = await information.save();
+        res.status(200).send(createinformation);
+      } else {
+        const information = InformationSchema({
+          title: req.body.title,
+          author: req.body.author,
+          desc: req.body.desc,
+          mention: req.body.mention,
+        });
+        const createinformation = await information.save();
+        res.status(200).send(createinformation);
+      }
+    } catch (e) {
+      res.status(400).send(e);
     }
-})
+  }
+);
 
-router.get("/getallinformation",async(req,res)=>{
-    try{
-        const allinformation = await InformationSchema.find();
-        res.status(200).send(allinformation)  
-    }catch(e){
-        res.status(400).send(e)
-    }
-})
+router.get("/getallinformation", async (req, res) => {
+  try {
+    const allinformation = await InformationSchema.find();
+    res.status(200).send(allinformation);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
-router.delete("/deleteinfromationbyid",async(req,res)=>{
-    try{
-        const deleteinformation = await InformationSchema.findByIdAndDelete(req.body.id)
-        res.status(200).send(deleteinformation)
-    }catch(e){
-        res.status(400).send(e)
-    }
-})
+router.delete("/deleteinfromationbyid", async (req, res) => {
+  try {
+    const deleteinformation = await InformationSchema.findByIdAndDelete(
+      req.body.id
+    );
+    res.status(200).send(deleteinformation);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
 module.exports = router;
--- help-desk/src/components/FacultyProfile/Postinformation/Postinformation.jsx
+++ help-desk/src/components/FacultyProfile/Postinformation/Postinformation.jsx
@@ -1,43 +1,141 @@
-import React from 'react';
+import React, { useState } from "react";
 import "./Postinformation.css";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
+import StoreInUsestate from "../../../util/StoreInUsestate";
+import axios from "axios";
 
 function Postinformation() {
+  const [infoData, setinfoData] = useState({
+    title: "",
+    desc: "",
+    author: "",
+    mention: "",
+  });
+
+  const [infoPhoto, setinfoPhoto] = useState("");
+
+  const haldleSubmit = (e) => {
+    e.preventDefault();
+    const InformationData = new FormData();
+    InformationData.append("author", localStorage.getItem("id"));
+    InformationData.append("title", infoData.title);
+    InformationData.append("desc", infoData.desc);
+    InformationData.append("mention", infoData.mention);
+    InformationData.append("infoPhoto", infoPhoto);
+    axios
+      .post("http://localhost:5000/postinformation", InformationData)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((e) => {
+        console.log(e, "err");
+      });
+  };
+
+  // TO discard the Filled Data
+  const discardFunction = () => {
+    setinfoData({
+      title: "",
+      desc: "",
+      author: "",
+      mention: "",
+    });
+    setinfoPhoto("");
+  };
+
   return (
     <>
-        <div className="postcontainer">
-            <div className='wholepostinfo'>
-                <div className='postheading'>
-                    <h3>Post New Information</h3>
-                </div>
-                <div className='postform'>
-                    <div className='postdetails'>
-                        <label>Information Title</label>
-                        <input type="text" className='post_input'/>
-                        <label>Information Description</label>
-                        <input type="text" className='post_input'/>
-                        <label>Class Or Persion Mention (@xyz)</label>
-                        <input type="text" className='post_input'/>
-                        <div className="postbutton">
-                            <button type="submit" className='pbutton'>Post</button>
-                            <button type="submit" className='pbutton'>Discard</button>
-                        </div>
-                    </div>
-                    <div className='postimage'>
-                        <label>Add Photo Here</label>
-                        <div className="pimageborder"></div>
-                        {/* <input
-                            className="postimageInput"
-                            type="file"
-                            id="postimageInput"
-                            name="postPhoto"
-                        /> */}
-                        <img src="./images/camera.png" alt="postcameraimg" className="postcameraimg"></img>
-                    </div>
-                </div>
+      <div className="postcontainer">
+        <div className="wholepostinfo">
+          <div className="postheading">
+            <h3>Post New Information</h3>
+          </div>
+          <div className="postform">
+            <div className="postdetails">
+              <label>Information Title</label>
+              <input
+                type="text"
+                className="post_input"
+                name="title"
+                value={infoData.title}
+                onChange={(e) => {
+                  StoreInUsestate.handleChange(e, setinfoData);
+                }}
+              />
+              <label>Information Description</label>
+              <input
+                type="text"
+                className="post_input"
+                name="desc"
+                value={infoData.desc}
+                onChange={(e) => {
+                  StoreInUsestate.handleChange(e, setinfoData);
+                }}
+              />
+              <label>Class Or All (@xyz) Mentions</label>
+              <input
+                type="text"
+                className="post_input"
+                name="mention"
+                value={infoData.mention}
+                onChange={(e) => {
+                  StoreInUsestate.handleChange(e, setinfoData);
+                }}
+              />
+              <div className="postbutton">
+                <button
+                  type="submit"
+                  className="pbutton"
+                  onClick={(e) => {
+                    haldleSubmit(e);
+                  }}
+                >
+                  Post
+                </button>
+                <button
+                  type="submit"
+                  className="pbutton"
+                  onClick={() => {
+                    discardFunction();
+                  }}
+                >
+                  Discard
+                </button>
+              </div>
             </div>
+            <div className="postimage">
+              <label htmlFor="postImageInput">
+                {infoPhoto ? (
+                  <img
+                    src={URL.createObjectURL(infoPhoto)}
+                    className="infoPhoto"
+                    alt="ComplaintPhoto"
+                  />
+                ) : (
+                  <>
+                    <p>Add Photo Here</p>
+                    <div className="pimageborder"></div>
+                  </>
+                )}
+                <input
+                  className="postImageInput"
+                  type="file"
+                  id="postImageInput"
+                  name="postPhoto"
+                  onChange={(e) => {
+                    setinfoPhoto(e.target.files[0]);
+                  }}
+                />
+                <div style={{ textAlign: "center" }}>
+                  <CameraAltIcon fontSize="large" sx={{ marginTop: "5px" }} />
+                </div>
+              </label>
+            </div>
+          </div>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Postinformation
+export default Postinformation;
